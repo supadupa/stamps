ab97bcf6db6adab94b77b9c549c852b3ab5a4f5d
Dan Garland <dan@dangarland.co.uk>
2013-12-12 20:32:43 +0000

    Adding a test for the custom lines

--- test/mapping_test.rb
+++ test/mapping_test.rb
@@ -30,4 +30,52 @@ class MappingTest < Test::Unit::TestCase
     end
 
   end
+
+  context 'customs item' do
+    setup do
+      @customs = Stamps::Mapping::Customs.new({
+      :content_type=>"Other",
+      :comments => 'What a lovely shipment',
+      :license_number => 'License 123',
+      :certificate_number => 'ABC 123',
+      :invoice_number => 'Invoice 123',
+      :other_describe=>"Other description.",
+      :customs_lines=> {
+        :custom => [
+          {:description=>"my product name 1",
+          :quantity=>1,
+          :value=>5.99,
+          :weight_lb=>0,
+          :weight_oz=>4,
+          :country_of_origin=>"US"},
+          {:description=>"my product name 2",
+          :quantity=>1,
+          :value=>5.99,
+          :weight_lb=>0,
+          :weight_oz=>4,
+          :country_of_origin=>"US"},
+          {:description=>"my product name 3",
+          :quantity=>1,
+          :value=>5.99,
+          :weight_lb=>0,
+          :weight_oz=>4,
+          :country_of_origin=>"US"}
+        ]
+      }}).to_hash
+    end
+
+    should 'map to CustomLine fields properly' do
+      assert_equal @customs['ContentType'], 'Other'
+      assert_equal @customs['Comments'], 'What a lovely shipment'
+      assert_equal @customs['LicenseNumber'], 'License 123'
+      assert_equal @customs['CertificateNumber'], 'ABC 123'
+      assert_equal @customs['InvoiceNumber'], 'Invoice 123'
+      assert_equal @customs['OtherDescribe'], 'Other description.'
+      assert_equal @customs['CustomsLines'].length, 1
+      assert_equal @customs['CustomsLines']["CustomsLine"].length, 3
+      assert_equal @customs['CustomsLines']["CustomsLine"][0]["Description"], "my product name 1"
+      assert_equal @customs['CustomsLines']["CustomsLine"][1]["Description"], "my product name 2"
+      assert_equal @customs['CustomsLines']["CustomsLine"][2]["Description"], "my product name 3"
+    end
+  end
 end
